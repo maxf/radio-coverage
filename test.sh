65f6046f5d4d53d86feb297da06644f2fe348cf2
Max Froumentin <max@froumentin.net>
2017-07-03 18:07:06 +0100

    Follow redirects and be verbose

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-curl -F geom=@couv.json -F callback="http://httpbin.org/post" http://localhost:8080/couverture.fcgi
+curl -v -L -F geom=@couv.json -F callback="http://httpbin.org/post" http://localhost:8080/couverture.fcgi
 
