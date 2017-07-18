8edd0b43321d49c0e354c84eed068bc4f4b5b66a
Max Froumentin <github@lapin-bl.eu>
2017-07-18 21:50:50 +0100

    Print labels, not just filenames

--- clip.py
+++ clip.py
@@ -22,28 +22,37 @@ def count_population_file(filename, geoms):
         return count_population(src, geoms)
 
 data_files = [
-"SEN14_A0005_adjv1.TIF", "SEN14_A2530_adjv1.TIF", "SEN14_A5055_adjv1.TIF",
-"SEN14_A0510_adjv1.TIF", "SEN14_A3035_adjv1.TIF", "SEN14_A5560_adjv1.TIF",
-"SEN14_A1015_adjv1.TIF", "SEN14_A3540_adjv1.TIF", "SEN14_A6065_adjv1.TIF",
-"SEN14_A1520_adjv1.TIF", "SEN14_A4045_adjv1.TIF", "SEN14_A65PL_adjv1.TIF",
-"SEN14_A2025_adjv1.TIF", "SEN14_A4550_adjv1.TIF"
+    { 'name': "SEN14_A0005_adjv1.TIF", 'label': " 0 -  5 ans" },
+    { 'name': "SEN14_A0510_adjv1.TIF", 'label': " 5 - 10 ans" },
+    { 'name': "SEN14_A1015_adjv1.TIF", 'label': "10 - 15 ans" },
+    { 'name': "SEN14_A1520_adjv1.TIF", 'label': "15 - 20 ans" },
+    { 'name': "SEN14_A2025_adjv1.TIF", 'label': "20 - 25 ans" },
+    { 'name': "SEN14_A2530_adjv1.TIF", 'label': "25 - 30 ans" },
+    { 'name': "SEN14_A3035_adjv1.TIF", 'label': "30 - 35 ans" },
+    { 'name': "SEN14_A3540_adjv1.TIF", 'label': "35 - 40 ans" },
+    { 'name': "SEN14_A4045_adjv1.TIF", 'label': "40 - 45 ans" },
+    { 'name': "SEN14_A4550_adjv1.TIF", 'label': "45 - 50 ans" },
+    { 'name': "SEN14_A5055_adjv1.TIF", 'label': "50 - 55 ans" },
+    { 'name': "SEN14_A5560_adjv1.TIF", 'label': "55 - 60 ans" },
+    { 'name': "SEN14_A6065_adjv1.TIF", 'label': "60 - 65 ans" },
+    { 'name': "SEN14_A65PL_adjv1.TIF", 'label': "65 ans et plus" }
 ]
 
 
-def count_all_populations(geoms, data_path, file = None):
+def count_all_populations(geoms, data_path, output = None):
     geometry = json.loads(geoms)
     total_population = {}
 
-    for filename in sorted(data_files):
-        if file:
-            file.write("processing %s\n" % filename)
-            file.flush()
-        full_path = data_path + '/' + filename
+    for file in sorted(data_files):
+        if output:
+            output.write("processing %s\n" % file['name'])
+            output.flush()
+        full_path = data_path + '/' + file['name']
         pop = count_population_file(full_path, geometry)
-        if file:
-            file.write("population: %d\n" % pop)
-            file.flush()
-        total_population[full_path] = str(pop)
+        if output:
+            output.write("%s: %d\n" % (file['label'], pop))
+            output.flush()
+        total_population[file['label']] = str(pop)
 
     return total_population
 
